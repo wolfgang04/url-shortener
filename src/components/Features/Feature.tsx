1e5e82c2742a4943c091df9342ea7c3827183660
Andrew <carlandrewlinao@gmail.com>
2023-05-17 21:10:38 +0800

    finalize styling the features section

--- src/components/Features/Feature.tsx
+++ src/components/Features/Feature.tsx
@@ -8,10 +8,17 @@ interface Props {
 
 const Feature: React.FC<Props> = (props) => {
 	return (
-		<div>
-			<div className="rounded-md bg-white p-4">
-				<h1 className="text-[2em] font-bold">{props.h1}</h1>
-				<p className="text-gray">{props.p}</p>
+		<div className="w-80">
+			<div className="relative flex flex-col items-center justify-center rounded-md bg-white p-5">
+				<div className="absolute top-0 flex h-[80px] w-[80px] -translate-y-1/2 items-center justify-center rounded-full bg-dark-Violet">
+					<img src={props.img} alt="" className="" />
+				</div>
+
+				<div className="mt-10">
+					<h1 className="text-[28px] font-bold">{props.h1}</h1>
+					<br />
+					<p className="text-gray">{props.p}</p>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -3,29 +3,45 @@ import Feature from "./Feature";
 
 const Features = () => {
 	return (
-		<div className="flex flex-col items-center justify-center gap-5 text-center ">
-			<h1 className="text-[2em] font-bold">Advanced Statistics</h1>
+		<div className="mb-20 flex flex-col items-center justify-center gap-20 bg-gray-bg text-center">
+			<div className="p-5">
+				<h1 className="text-[2em] font-bold">Advanced Statistics</h1>
+				<br />
 
-			<p className="text-gray">
-				Track how your links are performing across the web with our
-				advanced statistics dashboard
-			</p>
+				<p className="text-gray">
+					Track how your links are performing across the web with our
+					advanced statistics dashboard
+				</p>
+			</div>
 
-			<Feature
-				h1="Brand Recognition"
-				p="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instill confidence in your content."
-				img=""
-			/>
-			<Feature
-				h1="Detailed Records"
-				p="Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions"
-				img=""
-			/>
-			<Feature
-				h1="Fully Customizable"
-				p="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
-				img=""
-			/>
+			<div className="gap--24 flex flex-col items-center justify-center">
+				<Feature
+					h1="Brand Recognition"
+					p="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instill confidence in your content."
+					img={
+						process.env.PUBLIC_URL +
+						"/images/icon-brand-recognition.svg"
+					}
+				/>
+				<div className="h-20 border-r-8 border-cyan" />
+				<Feature
+					h1="Detailed Records"
+					p="Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions"
+					img={
+						process.env.PUBLIC_URL +
+						"/images/icon-detailed-records.svg"
+					}
+				/>
+				<div className="h-20 border-r-8 border-cyan" />
+				<Feature
+					h1="Fully Customizable"
+					p="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
+					img={
+						process.env.PUBLIC_URL +
+						"/images/icon-fully-customizable.svg"
+					}
+				/>
+			</div>
 		</div>
 	);
 };
