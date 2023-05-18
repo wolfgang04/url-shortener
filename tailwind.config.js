2d50f50197c3feabfbffd5b1a8622dbeb44a1c39
Andrew <carlandrewlinao@gmail.com>
2023-05-18 22:04:14 +0800

    update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,25 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
+			fontSize: {
+				'base': '18px',
+			},
 			colors: {
-				"cyan": "hsl(180, 66%, 49%)",
+				cyan: "hsl(180, 66%, 49%)",
 				"dark-Violet": "hsl(257, 27%, 26%)",
-				"gray": "hsl(0, 0%,75%)",
-				"hrayish-Violet": "hsl(257, 7%, 63%)",
+				gray: "hsl(0, 0%,75%)",
+				"grayish-Violet": "hsl(257, 7%, 63%)",
 				"very-Dark-Blue": "hsl(255, 11%, 22%)",
-				"very-Dark-Violet": "hsl(260, 8%,14%)"
-			}
+				"very-Dark-Violet": "hsl(260, 8%,14%)",
+				"gray-bg": "#F0F1F6",
+			},
+			backgroundImage: {
+				// "shorten": url,
+				// "shorten-mobile": "url('/images/bg-shorten-mobile.svg')",
+			},
 		},
 	},
 	plugins: [],
